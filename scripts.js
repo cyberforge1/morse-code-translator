5e1be87beb147b2b174d9e28a4123b9e7221f2a3
Oliver Jenkins <cyberforge1@gmail.com>
2024-03-31 13:15:48 +1100

    correct keydown inputs added

--- scripts.js
+++ scripts.js
@@ -48,58 +48,70 @@ const rightInput = document.querySelector('#rightInput')
 
 let rightInputInnerText = rightInput.innerText
 
+let currentWord = ''
 // ENGLISH TO MORSE CODE
-leftInput.addEventListener('input', () => {
-
-
-
-
-    let leftInputValue = leftInput.value
-    let currentLeftInputChar = leftInputValue[leftInputValue.length - 1]
-    rightInput.setAttribute('readonly', true);
-
-
-
-
-
-
-
-    // console.log('This is the left input', currentLeftInputChar.toUpperCase())
-    // console.log('The translated left input is', searchForMatchingKey(currentLeftInputChar.toUpperCase()))
-
-    // let totalLength = 
-    // let currentLength = 
-
-
-
-    if(currentLeftInputChar) {
-
-
-        let currentMorseChar = searchForMatchingKey(currentLeftInputChar.toUpperCase())
-        //console.log('This is the current morse code char', currentMorseChar)
-
-
-
-        rightInputInnerText += currentMorseChar
-        //console.log('This is the right input inner text:', rightInputInnerText)
-
-
-
-        rightInput.value = rightInputInnerText
+leftInput.addEventListener('keydown', (event) => {
+    if (event.key === " ") {
+        console.log("Space key pressed");
+        currentWord = '';
+    } else if (event.key === "Backspace") {
+        console.log("Backspace key pressed");
+        currentWord = currentWord.slice(0, -1);
+        console.log(currentWord)
+        console.log('This is the current word length', currentWord.length);
+    } else {
+        let leftInput = event.key.toUpperCase();
+        console.log("Key pressed: " + leftInput);
+        rightInput.setAttribute('readonly', true);
+        currentWord += leftInput;
+        console.log('This is the current word', currentWord);
+        console.log('This is the current word length', currentWord.length);
     }
-
-    if (leftInputValue.length === 0) {
-        rightInput.removeAttribute('readonly');
-        clearInputs()
-    }
-})
+});
 
 
 
-if (leftInput.value === '') {
-    //console.log('clear the right input')
-    rightInput.innerText = ''
-}
+
+
+
+
+
+//     // console.log('This is the left input', currentLeftInputChar.toUpperCase())
+//     // console.log('The translated left input is', searchForMatchingKey(currentLeftInputChar.toUpperCase()))
+
+//     // let totalLength = 
+//     // let currentLength = 
+
+
+
+//     if(currentLeftInputChar) {
+
+
+//         let currentMorseChar = searchForMatchingKey(currentLeftInputChar.toUpperCase())
+//         //console.log('This is the current morse code char', currentMorseChar)
+
+
+
+//         rightInputInnerText += currentMorseChar
+//         //console.log('This is the right input inner text:', rightInputInnerText)
+
+
+
+//         rightInput.value = rightInputInnerText
+//     }
+
+//     if (leftInputValue.length === 0) {
+//         rightInput.removeAttribute('readonly');
+//         clearInputs()
+//     }
+// })
+
+
+
+// if (leftInput.value === '') {
+//     //console.log('clear the right input')
+//     rightInput.innerText = ''
+// }
 
 
 
